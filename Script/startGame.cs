d05341a8883b0a54c7a7ab31ac20eb07a1a2bed5
scoshea <scoshea@yahoo.com>
2017-02-24 12:42:09 -0800

    add basic slider (money bar)
    only decrements automatically

---
+++ Script/startGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class startGame : MonoBehaviour
+{
+
+    public spawnTrash spawnFunc;
+    public Button startbutton;
+
+    void Start()
+    {
+        Button btn = startbutton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        spawnFunc.spawn();
+		//BarManager.currentValue -= 0.5F;
+        transform.FindChild("game start").gameObject.SetActive(false);
+    }
+}
