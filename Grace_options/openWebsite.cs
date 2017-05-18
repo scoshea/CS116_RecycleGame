cd55530e212c88081b1d08ff76fd8405659c4608
Grace <grko@ucsc.edu>
2017-05-18 12:40:36 -0700

    options screen and money bar sprites
    added website link to options screen

---
+++ Grace_options/openWebsite.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openWebsite : MonoBehaviour {
+
+	public void loadWebsite(){
+		Application.OpenURL ("trashtossmobile.com");
+		Debug.Log ("website opened");
+	}
+}
