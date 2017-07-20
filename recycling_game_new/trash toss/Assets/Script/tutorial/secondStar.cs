c6dde864c920b8721e08e754a2656f98041b069b
bruribe <30302610+bruribe@users.noreply.github.com>
2017-07-20 13:36:27 -0700

    Add files via upload

---
+++ recycling_game_new/trash toss/Assets/Script/tutorial/secondStar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secondStar : MonoBehaviour {
+
+    public static int DestroyedCount;
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+
+ 	
+	// Update is called once per frame
+	void Update () {
+        switch (difficultySettings.tutorialLevelCounter)
+        {
+            case 1:
+                if (DestroyedCount > difficultySettings.levelGoal + 6)
+                    gameObject.SetActive(false);
+                break;
+            case 2:
+                if (DestroyedCount > difficultySettings.levelGoal + 6)
+                    gameObject.SetActive(false);
+                break;
+            case 3:
+                if (DestroyedCount > difficultySettings.levelGoal + 6)
+                    gameObject.SetActive(false);
+                break;
+            case 4:
+                if (DestroyedCount > difficultySettings.levelGoal + 6)
+                    gameObject.SetActive(false);
+                break;
+            case 5:
+                if (DestroyedCount > difficultySettings.levelGoal + 6)
+                    gameObject.SetActive(false);
+                break;
+
+        }    
+    }
+}
